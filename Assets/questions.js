b0c06fbe073879005e71db6d55504f180fa0247b
mabarra22 <Aadil@Aadils-MBP.attlocal.net>
2021-12-26 11:55:58 -0600

    initial

---
+++ Assets/questions.js
@@ -0,0 +1,27 @@
+/**set the time to zero*/
+var timer = 75;
+var timeCount;
+/**this is the timer funtion which will start counting as soon as the quiz starts*/
+function setupTimer() {
+    timeCount = setInterval(function () {
+        timer--;
+        var timeReset = timeElement.textContent = "Time:" + " " + timer;
+       timer = timer;
+        if (timer <= 0) {         
+            clearInterval(timeCount);
+              
+            timeElement.textContent = timeReset;
+             
+        }
+    }, 1000)
+}
+ 
+/**  Here is the event listener to start the timer and hide the quiz button*/
+document.addEventListener("click", function (event) {
+    if (event.target === btnElement) {
+        wrapperElement.style.display = "none";
+        setupTimer()
+        displayQuestions();
+    }
+
+})
